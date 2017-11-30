e8f6430b54998881a936a2100f809c535b85facf
Frarese <ali.elwahsh@gmail.com>
2017-11-30 23:45:16 +0000

    Aggiunte altre funzioni aritmetiche

--- emu.c
+++ emu.c
@@ -14,6 +14,7 @@ uint8_t ac:1;
 uint8_t pad:3;
 } Flags;
 
+// State dei registri
 typedef struct State8080 {
 uint8_t a;
 uint8_t b;
@@ -32,28 +33,73 @@ uint8_t int_enable;
 void TrovaLavoro(State8080* state)
 {
 	//You fucked up
-	printf ("Error: Unimplemented istruction!\n;");
+	printf ("Error: Unimplemented istruction!\n");
 	exit(1);
 }
 
+int Parity(int a)
+{
+	return 0;
+}
 void Emulatore8080p(State8080* state)
 {
 	uint8_t *opcode = &state->memory[state->pc];
+	uint16_t answer;
 
 	switch(*opcode)
 	{
 		case 0x00:
-			{
+			
 				//printf("NOP\n");
 			       	break; //NOP
-			}
+			
 		case 0x01:
-			{
+			
 				state->c = opcode[1];
 				state->b = opcode[2];
 				state->pc+=2;
 				break;
-			}
+		case 0x02: TrovaLavoro(state); break;
+		case 0x03: TrovaLavoro(state); break;
+		case 0x04: answer = (uint16_t) state->b++;
+			   state->cc.z =((answer & 0xff)== 0);
+			   state->cc.s = ((answer & 0x80)!= 0);
+			   state->cc.cy =(answer >0xff);
+			   state->cc.p = Parity(answer & 0xff);
+			   break;
+		case 0x05: answer = (uint16_t) state->b--;
+		  	   state->cc.z =((answer & 0xff)== 0);
+			   state->cc.s = ((answer & 0x80)!= 0);
+			   state->cc.cy =(answer >0xff);
+			   state->cc.p = Parity(answer & 0xff);
+			   break;
+		case 0x06: state->b = opcode[1];
+			   state->pc++;
+			   break;
+		case 0x07: TrovaLavoro(state); break;
+		case 0x08: break;
+		case 0x09: TrovaLavoro(state); break;
+		case 0x0a: TrovaLavoro(state); break;
+		case 0x0b: TrovaLavoro(state); break;
+		case 0x0c:
+			   answer = (uint16_t) state->c++;
+			   state->cc.z =((answer & 0xff)== 0);
+			   state->cc.s = ((answer & 0x80)!= 0);
+			   state->cc.cy =(answer >0xff);
+			   state->cc.p = Parity(answer & 0xff);
+			   break;
+		case 0x0d:
+			   answer = (uint16_t) state->c--;
+		  	   state->cc.z =((answer & 0xff)== 0);
+			   state->cc.s = ((answer & 0x80)!= 0);
+			   state->cc.cy =(answer >0xff);
+			   state->cc.p = Parity(answer & 0xff);
+			   break;
+		case 0x0e: state->c = opcode[1];
+			   state->pc++;
+			   break;
+
+
 		default: TrovaLavoro(state);
 	}
 	state->pc+=1;
